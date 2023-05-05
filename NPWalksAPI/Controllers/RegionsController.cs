48c10de4c415e0cbceed34d0c2a133921a6ae53c
Shuvam-Upreti <shuvamupreti@gmail.com>
2023-05-05 13:06:55 +0545

    feat: Created Update Action method

--- NPWalksAPI/Controllers/RegionsController.cs
+++ NPWalksAPI/Controllers/RegionsController.cs
@@ -25,14 +25,14 @@ namespace HidaaiAPI.Controllers
 
             //Map Domain model to DTOs
             var regionsDto = new List<RegionDto>();
-            foreach(var regionDomain in regionsDomain)
+            foreach (var regionDomain in regionsDomain)
             {
                 regionsDto.Add(new RegionDto()
                 {
-                    Id= regionDomain.Id,
-                    Code=regionDomain.Code,
-                    Name=regionDomain.Name,
-                    RegionImageUrl=regionDomain.RegionImageUrl
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
                 });
             }
 
@@ -46,10 +46,10 @@ namespace HidaaiAPI.Controllers
         public IActionResult GetById([FromRoute] Guid id)
         {
             //Get from domain model
-            var regionDomain = dbContext.Regions.FirstOrDefault(x =>x.Id==id);
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
             if (regionDomain == null)
             {
-                return NotFound(); 
+                return NotFound();
             }
 
             //Map Domain to Dto
@@ -67,7 +67,7 @@ namespace HidaaiAPI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody]AddRegionRequestDto addRegionRequestDto)
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
             //Map Dto to Domain model
             var regionDomainModel = new Region
@@ -92,7 +92,35 @@ namespace HidaaiAPI.Controllers
 
             //return ma nameof(get by id deko kinaki create vayera created vayeko wala return garna paryo single
             //aarko new garera object banako jasma mathi ko created id ra banako naya ko id equal huna paryoo
-            return CreatedAtAction(nameof(GetById), new {id=regionDomainModel.Id}, regionDto);
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            //check id region exist
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //Map Dto to Domain Model
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+            dbContext.SaveChanges();
+
+            //Convert Domain model to Dto
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return Ok(regionDto);
         }
     }
 }
